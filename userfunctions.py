6a20796103f64632270b9f9015e5cab63ec75b9c
ShadowElf37 <ykey-cohen@emeryweiner.org>
2021-01-25 20:12:01 -0600

    BETA v0.2.0c wutface tcp

--- userfunctions.py
+++ userfunctions.py
@@ -12,4 +12,10 @@ def after(duration, f=(lambda *args, **kwargs: None), *args, **kwargs):
     Thread(target=exec, args=args, kwargs=kwargs, daemon=True).start()
 
 
+def fade_out(channel, duration):
+    original_gain = channel.gain
+    channel.fade_gain(-20, duration)
+    after(duration, channel.stop_all)
+    after(duration+0.1, channel.fade_gain, original_gain, 0)
+
 # eos = EOSIonXe(channeller.osc, '0.0.0.0')
